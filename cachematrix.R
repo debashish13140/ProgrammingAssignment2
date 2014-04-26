f9a9e2868819065f65d98b1c76d262e3a002e817
debashish13140 <debashish.jitu@gmail.com>
2014-04-27 00:04:08 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#the below function creates a matrix 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setval <- function(mean) m <<- mean
+        getval <- function() m
+        list(set = set, get = get,
+             setval = setval,
+             getval = getval)
 }
-
-
-## Write a short comment describing this function
+# the below function provides the cache value of the matrix if avaliable or else computes the new value
+# and returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getval()                             #query the x vector's cache
+        if(!is.null(m)) {                           #checks for cache
+                message("getting cached data")
+                return(m)                           #returns the avaliable cache value
+        }
+        data <- x$get()                             #compute if no value in cache
+        m <- solve(data, ...)                       # get the new inverse of the matrix
+        x$setval(m)
+        m                                           #return the result
 }
